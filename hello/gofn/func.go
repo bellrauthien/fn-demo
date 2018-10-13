358a815ebc60e250de9d1be19e5b59f32d06a8bb
Leonardo Gonzalez <lgonzalez@spsolutions.com.mx>
2018-10-13 12:26:38 -0500

    como estan

--- hello/gofn/func.go
+++ hello/gofn/func.go
@@ -23,7 +23,7 @@ func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
 	msg := struct {
 		Msg string `json:"message"`
 	}{
-		Msg: fmt.Sprintf("Hola amigos %s", p.Name),
+		Msg: fmt.Sprintf("Hola amigos como estan %s", p.Name),
 	}
 	json.NewEncoder(out).Encode(&msg)
 }
